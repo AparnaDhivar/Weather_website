da38eefebf8b5fe16b8c9ebf08e839b8acdd193b
Aparna Dhivar <aparnadhivar_ds_2020@ltce.in>
2023-07-31 22:55:15 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "daefe1b68f2b9b4321e308c987b8bece",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
